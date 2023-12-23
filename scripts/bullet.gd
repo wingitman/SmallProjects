9e572653245473593e03a05c7f4a7a563a6f4deb
wingitman <dlblackburn111@gmail.com>
2023-12-23 22:26:26 +0000

    bullet collission

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var direction = Vector2(1,0)
 var speed = 300
+var bounces = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered",_collide)
@@ -11,4 +12,14 @@ func _process(delta):
 	position += speed * direction * delta
 
 func _collide(body):
-	print("Collide: ",body.name)
+	print("Collide: ",body.name, " : ", self.get_parent().name)
+	if (body.name != self.get_parent().name) and bounces <= 0:
+		queue_free()
+	else:
+		var incidence = global_rotation - body.global_rotation
+		# Calculate the angle of reflection
+		var reflection = incidence
+		# Calculate the reflected rotation angle
+		var reflected_rotation = global_rotation - incidence + reflection
+		# Set the rotation of the reflected laser
+		global_rotation = reflected_rotation
