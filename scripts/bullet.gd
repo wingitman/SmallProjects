fd5d376ca801a5d600c7e054811d685c9feb6d7f
wingitman <dlblackburn111@gmail.com>
2023-12-25 00:18:53 +0000

    bullet and window fix
    fixed window rendering and anti aliasing

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,7 +2,11 @@ extends Area2D
 
 var direction = Vector2(1,0)
 var speed = 300
-var bounces = 3
+var velocity: = Vector2()
+var half_of_tile_size = 4
+
+const RAY_LENGTH = 1000
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered",_collide)
@@ -13,13 +17,6 @@ func _process(delta):
 
 func _collide(body):
 	print("Collide: ",body.name, " : ", self.get_parent().name)
-	if (body.name != self.get_parent().name) and bounces <= 0:
+	if (body.name != self.get_parent().name):
 		queue_free()
-	else:
-		var incidence = global_rotation - body.global_rotation
-		# Calculate the angle of reflection
-		var reflection = incidence
-		# Calculate the reflected rotation angle
-		var reflected_rotation = global_rotation - incidence + reflection
-		# Set the rotation of the reflected laser
-		global_rotation = reflected_rotation
+		
